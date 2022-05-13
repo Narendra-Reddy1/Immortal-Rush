93f30e160e6e5fee8e8af108054e9fd81be5b1de
Narendra-Reddy <58972366+Narendra-Reddy1@users.noreply.github.com>
2022-05-13 18:28:22 +0530

    Added Folder Structure

---
+++ Assets/_Game/_Scripts/InputManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+namespace Naren_Dev
+{
+    public class InputManager : MonoBehaviour
+    {
+
+        #region Singleton
+        public static InputManager instance { get; private set; }
+        #endregion
+
+        #region Variables
+
+        private ControlHub m_inputActions;
+        public Vector2 moveAxis = Vector2.zero;
+        
+        #endregion
+
+
+
+        private void OnEnable()
+        {
+            m_inputActions.Player.Movement.performed += _ => GetInput();
+            m_inputActions.Player.Movement.canceled += _ => GetInput();
+        }
+
+        private void OnDisable()
+        {
+            m_inputActions.Player.Movement.performed -= _ => GetInput();
+            m_inputActions.Player.Movement.canceled -= _ => GetInput();
+        }
+
+        private void Awake()
+        {
+            if (instance != this && instance != null) Destroy(gameObject);
+            else if (instance == null) instance = this;
+
+            m_inputActions = new ControlHub();
+            m_inputActions.Enable();
+        }
+
+        private void GetInput()
+        {
+            moveAxis = m_inputActions.Player.Movement.ReadValue<Vector2>();
+        }
+
+    }
+}
---
+++ Assets/_Game/_Scripts/Utils/SovereignUtils.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SovereignUtils
+{
+    public static void Log(object s)
+    {
+
+#if UNITY_EDITOR
+        Debug.Log(s);
+#endif
+    }
+
+    public static void LogError(object s)
+    {
+
+#if UNITY_EDITOR
+        Debug.LogError(s);
+#endif
+    }
+}
